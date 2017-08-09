a3b6874da0898ad042089c1942c5daa0b84adfb6
BubbleM <1870408473@qq.com>
2017-08-09 23:38:19 +0800

    return lyric while num equal 1

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -15,7 +15,8 @@ describe("测试描述", function(){
     it("return common lyric while num between 3 ~ 99", function(){
         let num = 99;
         let lyric = beersong(num);
-        let expect_string = '99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.';
+        let expect_string = '99 bottles of beer on the wall, 99 bottles of beer.\n' +
+            'Take one down and pass it around, 98 bottles of beer on the wall.';
         
         expect(lyric).to.equal(expect_string);
     });
@@ -23,7 +24,19 @@ describe("测试描述", function(){
     it("return 2 bottles lyric while num equal 2", function(){
         let num = 2;
         let lyric = beersong(num);
-        let expect_string = '2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.';
+        let expect_string = '2 bottles of beer on the wall, 2 bottles of beer.\n' +
+            'Take one down and pass it around, 1 bottle of beer on the wall.';
+
+        expect(lyric).to.equal(expect_string);
+    });
+
+    it("return lyric while num equal 1", function(){
+        let num = 1;
+        let lyric = beersong(num);
+        let expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n' +
+            'Take one down and pass it around, no more bottles of beer on the wall.\n' +
+            'No more bottles of beer on the wall, no more bottles of beer.\n' +
+            'Go to the store and buy some more, 99 bottles of beer on the wall.';
 
         expect(lyric).to.equal(expect_string);
     });
