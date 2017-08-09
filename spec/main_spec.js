8dca7a586b5a4007a6a375b8d9b91372f0934db1
BubbleM <1870408473@qq.com>
2017-08-09 23:56:55 +0800

    refactor

--- spec/main_spec.js
+++ spec/main_spec.js
@@ -8,29 +8,28 @@ chai.use(sinonChai);
 
 var beersong = require("../lib/beersong.js");
 
-
 describe("测试描述", function(){
     sinon.spy(console, 'log');
 
-    it("return common lyric while num between 3 ~ 99", function(){
+    it("add common lyric while num between 3 ~ 99", function(){
         let num = 99;
         let lyric = beersong(num);
         let expect_string = '99 bottles of beer on the wall, 99 bottles of beer.\n' +
-            'Take one down and pass it around, 98 bottles of beer on the wall.';
+            'Take one down and pass it around, 98 bottles of beer on the wall.\n';
         
         expect(lyric).to.equal(expect_string);
     });
 
-    it("return 2 bottles lyric while num equal 2", function(){
+    it("add lyric while num equal 2", function(){
         let num = 2;
         let lyric = beersong(num);
         let expect_string = '2 bottles of beer on the wall, 2 bottles of beer.\n' +
-            'Take one down and pass it around, 1 bottle of beer on the wall.';
+            'Take one down and pass it around, 1 bottle of beer on the wall.\n';
 
         expect(lyric).to.equal(expect_string);
     });
 
-    it("return lyric while num equal 1", function(){
+    it("add lyric while num equal 1", function(){
         let num = 1;
         let lyric = beersong(num);
         let expect_string = '1 bottle of beer on the wall, 1 bottle of beer.\n' +
